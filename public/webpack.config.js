28f6fbe83dde46d8f2de940d29758da5b053ea57
Jason Shin <visualbbasic@gmail.com>
2016-08-18 15:35:58 +1000

    Package dependencies updated

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -5,6 +5,19 @@ module.exports = {
     context: __dirname,
     devtool: debug ? 'inline-sourcemap' : null,
     entry: './js/client.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0'],
+                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+                }
+            }
+        ]
+    },
     output: {
         path: __dirname + '/js',
         filename: 'client.min.js'
