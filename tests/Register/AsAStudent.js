7b3e6171c381afd0631586e42128641860af4420
Jason Shin <visualbbasic@gmail.com>
2016-09-03 22:29:28 +1000

    Removed JS DOM and Mocha DOM reference from tests: it was needed for jquery ajax but now we are using request module

--- tests/Register/AsAStudent.js
+++ tests/Register/AsAStudent.js
@@ -1,8 +1,5 @@
 import {chai, assert} from 'chai';
-var jsdom = require('mocha-jsdom'); // This is necessary for testing jQuery in Mocha
-jsdom();                            //Initiate jsdom
 import {register} from '../../public/js/api/student/StudentAPI';
-
 
 describe('Registering as a student', function(){
     this.timeout(300000);
@@ -11,15 +8,15 @@ describe('Registering as a student', function(){
 
     });
 
-    it('should fail at registering as a student Jason Shin', (done) => {
-
+    it('should fail at registering as a student Jason Shin', function(done){
+        console.log('testing ajax features');
         register()
-            .then((data) => {
-                console.log('succes: ' , data);
+            .then((body) => {
+                console.log(body);
                 done();
             })
-            .catch((data) => {
-                console.log('error: ' , data);
+            .catch((err) => {
+                console.log(err);
                 done();
             });
     });
