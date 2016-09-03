f18caf24d958df0e5afab7c4fb75524adb28ff98
Jason Shin <visualbbasic@gmail.com>
2016-09-03 11:25:59 +1000

    Implementing chai, which has mocha + should.

---
+++ tests/Register/AsAStudent.js
@@ -0,0 +1,16 @@
+import {chai, assert} from 'chai';
+
+describe('Registering as a student', function(){
+    this.timeout(300000);
+    before(function(){
+
+    });
+
+    it('should fail at registering as a student Jason Shin', function(){
+        assert.strictEqual('jason', 'shin');
+    });
+
+    after(function(){
+
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,7 @@ module.exports = {
                 test: /\.js?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel'
-                /*query: {
-                    presets: ['react', 'es2015', 'stage-0'],
-                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
-                }*/
+
             },
             {
                 test: /\.scss$/,
