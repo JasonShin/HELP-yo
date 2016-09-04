bac453f60fa5d49e03861b62478f1a5f4b3d846d
Jason Shin <visualbbasic@gmail.com>
2016-09-04 22:34:25 +1000

    Redirection after login works and after login you can go into bookings/history page too. However, it required upgrade to react-router 2.4.0. Now it gives a warning browser.js:49Warning: [react-router] It appears you have provided a deprecated history object to `<Router/>`, please use a history provided by React Router with `import { browserHistory } from 'react-router'` or `import { hashHistory } from 'react-router'`. If you are using a custom history please create it with `useRouterHistory`, see http://tiny.cc/router-usinghistory for details.

--- public/js/pages/Login.js
+++ public/js/pages/Login.js
@@ -1,35 +1,36 @@
 import React from 'react';
+import { browserHistory, withRouter } from 'react-router'
 import TodoList from '../components/TodoList';
 import TodoStore from '../stores/TodoStore';
+import {registerFirebase,loginFirebase} from '../api/student/StudentAPI';
 
-//import {registerFirebase,loginFirebase} from '../api/student/StudentAPI';
+class Login extends React.Component {
 
-export default class Login extends React.Component {
+
 
     handleSubmit(e) {
         e.preventDefault();
         var email = this.emailField.value;
         var password = this.passwordField.value;
 
-
-
-        /*loginFirebase({
+        loginFirebase({
             email: email,
             password: password
          }).
          then((body) => {
             console.log(body);
-            this.context.router.replace('/');
+           // this.context.router.replace('/');
+            this.props.router.push('/');
          }).
          catch((err) => {
             console.log(err);
-         });*/
+         });
     }
 
     render() {
         return (
             <div>
-                <h1>You must login to goto any other pages!</h1>
+                <h1>You must login to goto any other pages222</h1>
 
                 <form onSubmit={this.handleSubmit.bind(this)}>
                     <div class="form-group">
@@ -49,3 +50,5 @@ export default class Login extends React.Component {
         );
     }
 }
+
+export default withRouter(Login);
