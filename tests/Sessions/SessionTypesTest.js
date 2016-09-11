5998e913ceb1ccb41dbb6acc434b3c23d6a5b1b1
Jason Shin <visualbbasic@gmail.com>
2016-09-11 16:27:14 +1000

    Sessions search (Filter) location, date, tutor, topic complete

--- tests/Sessions/SessionTypesTest.js
+++ tests/Sessions/SessionTypesTest.js
@@ -1,18 +1,24 @@
 import {chai, assert} from 'chai';
-import HELPFirebase from '../../public/js/api/firebase.api';
-import {getAllSessionsTypes} from '../../public/js/api/sessions.api';
+//import HELPFirebase from '../../public/js/api/firebase.api';
+import {getAllSessionsTypes, searchSessionBookingsByDate, searchSessionBookingsByLocation} from '../../public/js/api/sessions.api';
 
 
 describe('Retreiving session types', function() {
     this.timeout(20000);
 
-    it('should retrieve all sessions types', function() {
-        getAllSessionsTypes()
-            .then((body) => {
-                console.log(body);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+    it('should retrieve all sessions types', async() => {
+
+        const response = await getAllSessionsTypes();
+    });
+
+    it('should retreive all sessions from date from 2007-08-01T00:00:00 to 2007-08-10T00:00:00', async() => {
+
+        const opt = {
+            'studentId': '11610895',
+            'startingDtBegin': '2007-08-01T00:00:00',
+            'endingDtBegin': '2007-08-10T00:00:00'
+        };
+
+        const response = await searchSessionBookingsByDate(opt);
     });
 });
