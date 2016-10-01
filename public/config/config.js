41ccc2c9910e18a7f4135675604c4fd2d7fce784
JasonShin <yong.j.shin@student.uts.edu.au>
2016-10-01 18:26:49 +1000

    Delete config.js

--- public/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	baseURL: 'https://api.utsapi.me/api/',
-	appKey: 'd08f1438a39947458d02a70fab255cfd',
-	firebaseURL: 'https://helps-uts.firebaseio.com',
-	titleEnding: ' - UTS HELPS',
-	webpackEnv: 'development'
-};
-
-//export default config;
